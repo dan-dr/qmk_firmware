d40d7cff4fc2c66aac5ac5e61e898f2abebb488f
Dan Rosenshain <danrosenshain@gmail.com>
2022-04-30 14:17:52 +0300

    some more changes

--- keyboards/sofle/keymaps/danr/keymap.c
+++ keyboards/sofle/keymaps/danr/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
     KC_GRV, KC_ASTR, KC_P7, KC_P8, KC_P9, KC_PPLS,                          KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
-    KC_TRNS, KC_MINS, KC_P4, KC_P5, KC_P6, KC_PMNS,                         KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
+    KC_TRNS, KC_MINS, KC_P4, KC_P5, KC_P6, KC_PMNS,                         KC_PGDN, KC_NO, KC_NO, KC_NO, KC_DEL, KC_TRNS, \
     KC_TRNS, KC_EQL, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_TRNS,             KC_TRNS, KC_CAPS, KC_NO, KC_NO, KC_NO, KC_BSLS, KC_TRNS, \
                     KC_P0, KC_PDOT, KC_PENT, KC_TRNS, KC_TRNS,      KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS),
 
@@ -145,9 +145,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT( \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
     KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                  KC_PGUP, LCTL(KC_LEFT), KC_UP, LCTL(KC_RGHT), LCTL(KC_BSPC), KC_BSPC, \
-    KC_TRNS, KC_SCLN, KC_LCBR, KC_LPRN, KC_LBRC, KC_LT,                 KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, KC_BSPC, \
+    KC_TRNS, KC_SCLN, KC_LCBR, KC_LPRN, KC_LBRC, KC_LT,                 KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_BSPC, \
     KC_TRNS, KC_DOT, KC_RCBR, KC_RPRN, KC_RBRC, KC_GT, KC_TRNS,      KC_TRNS, KC_NO, KC_LSTRT, KC_NO, KC_LEND, KC_NO, KC_TRNS, \
                     KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
@@ -175,9 +175,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = LAYOUT( \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-    RESET, KC_NO, KC_QWERTY, KC_COLEMAK_DH, CG_SWAP, KC_NO,     KC_NO, KC_LSTRT, KC_NO, KC_LEND, KC_NO, KC_NO, \
+    RESET, KC_NO, KC_QWERTY, KC_COLEMAK_DH, CG_TOGG, KC_NO,     KC_NO, KC_LSTRT, KC_NO, KC_LEND, KC_NO, KC_NO, \
     KC_LAYOUT, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS,             KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO, \
-    CG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_PLUS, KC_NO,      KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, \
+    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_PLUS, KC_NO,      KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, \
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 
 };
