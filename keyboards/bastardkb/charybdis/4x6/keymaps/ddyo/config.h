17ecfcbaca19e1dbb7e1afb279a126fe896fc69d
ddyo <danrosenshain@gmail.com>
2026-07-07 18:39:26 +0300

    feat: base bkb-master-ddyo on bkb-master

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/ddyo/config.h
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#endif // VIA_ENABLE
+
+/* Disable unused features. */
+#define NO_ACTION_ONESHOT
+
+/* Charybdis-specific features. */
+
+/* Custom Splinky v3 wiring. */
+#undef MATRIX_ROW_PINS
+#define MATRIX_ROW_PINS \
+    { GP15, GP12, GP5, GP4, GP9 }
+
+#undef MATRIX_COL_PINS
+#define MATRIX_COL_PINS \
+    { GP13, GP14, GP16, GP6, GP7, GP8 }
+
+#define MATRIX_ROW_PINS_RIGHT \
+    { GP13, GP16, GP5, GP4, GP9 }
+
+#define MATRIX_COL_PINS_RIGHT \
+    { GP15, GP14, GP12, GP6, GP7, GP8 }
+
+#undef POINTING_DEVICE_CS_PIN
+#define POINTING_DEVICE_CS_PIN GP21
+
+#undef USB_VBUS_PIN
+#define USB_VBUS_PIN GP19
+
+#undef RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the trackball.  See also:
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1500
+// #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 1
+
+#endif // POINTING_DEVICE_ENABLE
+
+
+// #define EE_HANDS // not working well, also not recommended to connect the non trackball half
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+#undef ROTATIONAL_TRANSFORM_ANGLE
+#define ROTATIONAL_TRANSFORM_ANGLE -85
+
+#define FSR_ENABLE 1
+#define FSR_PIN GP26
+#define FSR_THRESHOLD 150
+#define FSR_RELEASE_THRESHOLD 80
+#define FSR_SCAN_INTERVAL_MS 20
+#define FSR_DEBUG_INTERVAL_MS 100
+
+#undef ADC_RESOLUTION
+#define ADC_RESOLUTION ADC_CFGR1_RES_12BIT
+
+#ifdef CONSOLE_ENABLE
+#define DDYO_DEBUG
+#endif
+
+#undef SPLIT_MAX_CONNECTION_ERRORS
+#define SPLIT_MAX_CONNECTION_ERRORS 0
